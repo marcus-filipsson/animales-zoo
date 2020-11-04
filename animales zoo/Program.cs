a69d54df79bddaac11965a17154a83b2c821ccfc
Marcus Filipsson <macke2002@outlook.com>
2020-11-04 09:21:52 +0100

    Add project files.

---
+++ animales zoo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animales_zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] allAnimals;
+        }
+    }
+}
